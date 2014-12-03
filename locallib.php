672894bce7eb91b1d0c8f73eda1f110d0e10304c
tobias.reischmann <tobias.reischmann@wi.uni-muenster.de>
2014-12-03 17:25:54 +0100

    Pulled the additional if statements and notifications in the processing functions and fixed using a deprecated public function

--- locallib.php
+++ locallib.php
@@ -256,107 +256,114 @@ class ratingallocate {
     }
     
     private function process_publish_allocations(){
-        global $USER;
-        $this->origdbrecord->{this_db\ratingallocate::PUBLISHED}   = true;
-        $this->origdbrecord->{this_db\ratingallocate::PUBLISHDATE} = time();
-        $this->origdbrecord->{this_db\ratingallocate::NOTIFICATIONSEND} = -1;
-        $this->ratingallocate            = new ratingallocate_db_wrapper($this->origdbrecord);
-        $this->db->update_record(this_db\ratingallocate::TABLE, $this->origdbrecord);
-        
-        // create the instance
-        $domination = new mod_ratingallocate\task\send_distribution_notification();
-        // set blocking if required (it probably isn't)
-        // $domination->set_blocking(true);
-        // add custom data
-        $domination->set_component('mod_ratingallocate');
-        $domination->set_custom_data(array(
-                        'userid' => $USER->id, // will be the sending user
-                        'ratingallocateid' => $this->ratingallocateid
-        ));
-        
-        // queue it
-        \core\task\manager::queue_adhoc_task($domination);
-        
-        //Logging
-        $event = \mod_ratingallocate\event\allocation_published::create_simple(
-                context_course::instance($this->course->id), $this->ratingallocateid, $this->get_allocations_for_logging());
-        $event->trigger();
-        
-        $output .= $OUTPUT->notification( get_string('distribution_published', ratingallocate_MOD_NAME), 'notifysuccess');
-        return $output;
+        if ($this->ratingallocate->accesstimestop < $now){
+            global $USER;
+    
+            $this->origdbrecord->{this_db\ratingallocate::PUBLISHED}   = true;
+            $this->origdbrecord->{this_db\ratingallocate::PUBLISHDATE} = time();
+            $this->origdbrecord->{this_db\ratingallocate::NOTIFICATIONSEND} = -1;
+            $this->ratingallocate            = new ratingallocate_db_wrapper($this->origdbrecord);
+            $this->db->update_record(this_db\ratingallocate::TABLE, $this->origdbrecord);
+            
+            // create the instance
+            $domination = new mod_ratingallocate\task\send_distribution_notification();
+            // set blocking if required (it probably isn't)
+            // $domination->set_blocking(true);
+            // add custom data
+            $domination->set_component('mod_ratingallocate');
+            $domination->set_custom_data(array(
+                            'userid' => $USER->id, // will be the sending user
+                            'ratingallocateid' => $this->ratingallocateid
+            ));
+            
+            // queue it
+            \core\task\manager::queue_adhoc_task($domination);
+            
+            //Logging
+            $event = \mod_ratingallocate\event\allocation_published::create_simple(
+                    context_course::instance($this->course->id), $this->ratingallocateid, $this->get_allocations_for_logging());
+            $event->trigger();
+            
+            $output .= $OUTPUT->notification( get_string('distribution_published', ratingallocate_MOD_NAME), 'notifysuccess');
+            return $output;
+        }
     }
     
     private function process_action_allocation_to_grouping(){
-        $allgroupings = groups_get_all_groupings($this->course->id);
-        $groupingidname = ratingallocate_MOD_NAME . '_instid_' . $this->ratingallocateid;
-        // search if there is already a grouping from us
-        $grouping = groups_get_grouping_by_idnumber($this->course->id, $groupingidname);
-        $groupingid = null;
-        if (!$grouping) {
-            // create grouping
-            $data = new stdClass();
-            $data->name = 'created from ' . $this->ratingallocate->name;
-            $data->idnumber = $groupingidname;
-            $data->courseid = $this->course->id;
-            $groupingid = groups_create_grouping($data);
-        } else {
-            $groupingid = $grouping->id;
-        }
-        
-        $group_identifier_from_choice_id = function ($choiceid) {
-            return ratingallocate_MOD_NAME . '_c_' . $choiceid;
-        };
-        
-        $choices = $this->get_choices_with_allocationcount();
-        
-        // make a new array containing only the identifiers of the choices
-        $choice_identifiers = array();
-        foreach ($choices as $id => $choice) {
-            $choice_identifiers[$group_identifier_from_choice_id($choice->id)] = array('key' => $id
-            );
-        }
-        
-        // find all associated groups in this grouping
-        $groups = groups_get_all_groups($this->course->id, 0, $groupingid);
-        
-        // loop through the groups in the grouping: if the choice does not exist anymore -> delete
-        // otherwise mark it
-        foreach ($groups as $group) {
-            if (array_key_exists($group->idnumber, $choice_identifiers)) {
-                // group exists, mark
-                $choice_identifiers[$group->idnumber]['exists'] = true;
-                $choice_identifiers[$group->idnumber]['groupid'] = $group->id;
-            } else {
-                // delete group $group->id
-                groups_delete_group($group->id);
-            }
-        }
-        
-        // create groups groups for new identifiers or empty group if it exists
-        foreach ($choice_identifiers as $group_idnumber => $choice) {
-            if (key_exists('exists', $choice)) {
-                // remove all members
-                groups_delete_group_members_by_group($choice['groupid']);
-            } else {
+        if ($this->ratingallocate->accesstimestop < $now){
+            $allgroupings = groups_get_all_groupings($this->course->id);
+            $groupingidname = ratingallocate_MOD_NAME . '_instid_' . $this->ratingallocateid;
+            // search if there is already a grouping from us
+            $grouping = groups_get_grouping_by_idnumber($this->course->id, $groupingidname);
+            $groupingid = null;
+            if (!$grouping) {
+                // create grouping
                 $data = new stdClass();
+                $data->name = 'created from ' . $this->ratingallocate->name;
+                $data->idnumber = $groupingidname;
                 $data->courseid = $this->course->id;
-                $data->name = $choices[$choice['key']]->title;
-                $data->idnumber = $group_idnumber;
-                $createdid = groups_create_group($data);
-                groups_assign_grouping($groupingid, $createdid);
-                $choice_identifiers[$group_idnumber]['groupid'] = $createdid;
+                $groupingid = groups_create_grouping($data);
+            } else {
+                $groupingid = $grouping->id;
             }
+            
+            $group_identifier_from_choice_id = function ($choiceid) {
+                return ratingallocate_MOD_NAME . '_c_' . $choiceid;
+            };
+            
+            $choices = $this->get_choices_with_allocationcount();
+            
+            // make a new array containing only the identifiers of the choices
+            $choice_identifiers = array();
+            foreach ($choices as $id => $choice) {
+                $choice_identifiers[$group_identifier_from_choice_id($choice->id)] = array('key' => $id
+                );
+            }
+            
+            // find all associated groups in this grouping
+            $groups = groups_get_all_groups($this->course->id, 0, $groupingid);
+            
+            // loop through the groups in the grouping: if the choice does not exist anymore -> delete
+            // otherwise mark it
+            foreach ($groups as $group) {
+                if (array_key_exists($group->idnumber, $choice_identifiers)) {
+                    // group exists, mark
+                    $choice_identifiers[$group->idnumber]['exists'] = true;
+                    $choice_identifiers[$group->idnumber]['groupid'] = $group->id;
+                } else {
+                    // delete group $group->id
+                    groups_delete_group($group->id);
+                }
+            }
+            
+            // create groups groups for new identifiers or empty group if it exists
+            foreach ($choice_identifiers as $group_idnumber => $choice) {
+                if (key_exists('exists', $choice)) {
+                    // remove all members
+                    groups_delete_group_members_by_group($choice['groupid']);
+                } else {
+                    $data = new stdClass();
+                    $data->courseid = $this->course->id;
+                    $data->name = $choices[$choice['key']]->title;
+                    $data->idnumber = $group_idnumber;
+                    $createdid = groups_create_group($data);
+                    groups_assign_grouping($groupingid, $createdid);
+                    $choice_identifiers[$group_idnumber]['groupid'] = $createdid;
+                }
+            }
+            
+            // add all participants in the correct group
+            $allocations = $this->get_all_allocations();
+            foreach ($allocations as $userid => $choice) {
+                $choice_id = array_keys($choice)[0];
+                $choiceidnumber = $group_identifier_from_choice_id($choice_id);
+                groups_add_member($choice_identifiers[$choiceidnumber]['groupid'], $userid);
+            }
+            // Invalidate the grouping cache for the course
+            cache_helper::invalidate_by_definition('core', 'groupdata', array(), array($this->course->id));
+            $output .= $OUTPUT->notification( get_string('moodlegroups_created', ratingallocate_MOD_NAME), 'notifysuccess');
+            return $output;
         }
-        
-        // add all participants in the correct group
-        $allocations = $this->get_all_allocations();
-        foreach ($allocations as $userid => $choice) {
-            $choice_id = array_keys($choice)[0];
-            $choiceidnumber = $group_identifier_from_choice_id($choice_id);
-            groups_add_member($choice_identifiers[$choiceidnumber]['groupid'], $userid);
-        }
-        // Invalidate the grouping cache for the course
-        cache_helper::invalidate_by_definition('core', 'groupdata', array(), array($this->course->id));
     }
     
     /**
@@ -399,12 +406,11 @@ class ratingallocate {
         if ($action === RATING_ALLOC_ACTION_RATE) {
             $output .= $this->process_rating_alloc_action_rate($action); 
         }
-        if ($this->ratingallocate->accesstimestop < $now && $action == ACTION_PUBLISH_ALLOCATIONS) {
+        if ($action == ACTION_PUBLISH_ALLOCATIONS) {
             $output .= $this->process_publish_allocations();
         }
-        if ($this->ratingallocate->accesstimestop < $now && $action == ACTION_ALLOCATION_TO_GROUPING) {
-            $this->create_moodle_groups();
-            $output .= $OUTPUT->notification( get_string('moodlegroups_created', ratingallocate_MOD_NAME), 'notifysuccess');
+        if ($action == ACTION_ALLOCATION_TO_GROUPING) {
+            $output .= $this->process_action_allocation_to_grouping();
         }
         if ($action == ACTION_ALLOCATE_PROCESS_MANUALFORM){
             $output .= $this->process_action_allocate_process_manualform();
