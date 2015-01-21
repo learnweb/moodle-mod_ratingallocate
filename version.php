1a36186219c716ab1d0edf195a5aa94b53b4b9bc
tobias.reischmann <tobias.reischmann@wi.uni-muenster.de>
2015-01-21 14:37:57 +0100

    Increased version number

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014120201;        // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2015011501;        // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2014051200;        // Requires this Moodle version
 $plugin->cron      = 0;                 // Period for cron to check this module (secs)
 $plugin->maturity  = MATURITY_BETA;
