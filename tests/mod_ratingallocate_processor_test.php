a355eb6d88b37848d6820994a6c4d97408b4298d
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2016-07-01 14:49:18 +0200

    Refactoring

--- tests/mod_ratingallocate_processor_test.php
+++ tests/mod_ratingallocate_processor_test.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * mod_ratingallocate processor tests
+ * Tests the internal processor, which controls the different steps of the workflow.
  *
  * @package    mod_ratingallocate
  * @category   test
@@ -24,66 +24,66 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class mod_ratingallocate_processor_testcase extends advanced_testcase {
-    
-        function setUp() {
+
+    public function setUp() {
             global $PAGE;
             $PAGE->set_url('/');
-        }
-    
-        /**
-         * Tests if process_publish_allocations is not working before time runs out
-         * Assert, that the ratingallocate can not be published
-         */
-        public function test_publishing_before_accesstimestop(){
-            $ratingallocate = mod_ratingallocate_generator::get_open_ratingallocate_for_teacher($this);
-            $this->assertEquals(0,$ratingallocate->ratingallocate->published);
-            $this->call_private_ratingallocate_method($ratingallocate,'process_publish_allocations');
-            $this->assertEquals(0,$ratingallocate->ratingallocate->published);
-        }
-        /**
-         * Tests if process_publish_allocations is working after time runs out
-         * Assert, that the ratingallocate can be published
-         */
-        public function test_publishing_after_accesstimestop(){
-            $ratingallocate = mod_ratingallocate_generator::get_closed_ratingallocate_for_teacher($this);
-            $this->assertEquals(0,$ratingallocate->ratingallocate->published);
-            $this->call_private_ratingallocate_method($ratingallocate,'process_publish_allocations');
-            $this->assertEquals(1,$ratingallocate->ratingallocate->published);
-        }
-        /**
-         * Tests if process_action_allocation_to_grouping is working before time runs out
-         * Assert, that the number of groupings does not change
-         */
-        public function test_grouping_before_accesstimestop(){
-            global $DB;
-            $ratingallocate = mod_ratingallocate_generator::get_open_ratingallocate_for_teacher($this);
-            $this->assertEquals(0, $DB->count_records('groupings'));
-            $this->call_private_ratingallocate_method($ratingallocate,'process_action_allocation_to_grouping');
-            $this->assertEquals(0, $DB->count_records('groupings'));
-        }
-        /**
-         * Tests if process_action_allocation_to_grouping is working after time runs out
-         * Assert, that the number of groupings changes as expected (1 Grouping should be created)
-         */
-        public function test_grouping_after_accesstimestop(){
-            global $DB;
-            $ratingallocate = mod_ratingallocate_generator::get_closed_ratingallocate_for_teacher($this);
-            $this->assertEquals(0, $DB->count_records('groupings'));
-            $this->call_private_ratingallocate_method($ratingallocate,'process_action_allocation_to_grouping');
-            $this->assertEquals(1, $DB->count_records('groupings'));
-        }
-        
-        /**
-         * Enables for calling the private processing functions of the ratingallocate
-         * @param ratingallocate $ratingallocate
-         * @param unknown $method_name name of private or protected method
-         * @param unknown $args arguments the method should be called with
-         */
-        private function call_private_ratingallocate_method(ratingallocate $ratingallocate, $method_name, $args=[]){
-            $class = new ReflectionClass('ratingallocate');
-            $method = $class->getMethod($method_name);
-            $method->setAccessible(true);
-            $method->invokeArgs($ratingallocate, $args);
-        }
-    
+    }
+
+    /**
+     * Tests if process_publish_allocations is not working before time runs out
+     * Assert, that the ratingallocate can not be published
+     */
+    public function test_publishing_before_accesstimestop() {
+        $ratingallocate = mod_ratingallocate_generator::get_open_ratingallocate_for_teacher($this);
+        $this->assertEquals(0, $ratingallocate->ratingallocate->published);
+        $this->call_private_ratingallocate_method($ratingallocate, 'process_publish_allocations');
+        $this->assertEquals(0, $ratingallocate->ratingallocate->published);
+    }
+    /**
+     * Tests if process_publish_allocations is working after time runs out
+     * Assert, that the ratingallocate can be published
+     */
+    public function test_publishing_after_accesstimestop() {
+        $ratingallocate = mod_ratingallocate_generator::get_closed_ratingallocate_for_teacher($this);
+        $this->assertEquals(0, $ratingallocate->ratingallocate->published);
+        $this->call_private_ratingallocate_method($ratingallocate, 'process_publish_allocations');
+        $this->assertEquals(1, $ratingallocate->ratingallocate->published);
+    }
+    /**
+     * Tests if process_action_allocation_to_grouping is working before time runs out
+     * Assert, that the number of groupings does not change
+     */
+    public function test_grouping_before_accesstimestop() {
+        global $DB;
+        $ratingallocate = mod_ratingallocate_generator::get_open_ratingallocate_for_teacher($this);
+        $this->assertEquals(0, $DB->count_records('groupings'));
+        $this->call_private_ratingallocate_method($ratingallocate, 'process_action_allocation_to_grouping');
+        $this->assertEquals(0, $DB->count_records('groupings'));
+    }
+    /**
+     * Tests if process_action_allocation_to_grouping is working after time runs out
+     * Assert, that the number of groupings changes as expected (1 Grouping should be created)
+     */
+    public function test_grouping_after_accesstimestop() {
+        global $DB;
+        $ratingallocate = mod_ratingallocate_generator::get_closed_ratingallocate_for_teacher($this);
+        $this->assertEquals(0, $DB->count_records('groupings'));
+        $this->call_private_ratingallocate_method($ratingallocate, 'process_action_allocation_to_grouping');
+        $this->assertEquals(1, $DB->count_records('groupings'));
+    }
+
+    /**
+     * Enables for calling the private processing functions of the ratingallocate
+     * @param ratingallocate $ratingallocate
+     * @param string $methodname name of private or protected method
+     * @param array $args arguments the method should be called with
+     */
+    private function call_private_ratingallocate_method(ratingallocate $ratingallocate, $methodname, $args=[]) {
+        $class = new ReflectionClass('ratingallocate');
+        $method = $class->getMethod($methodname);
+        $method->setAccessible(true);
+        $method->invokeArgs($ratingallocate, $args);
+    }
+
 }
