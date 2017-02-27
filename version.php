19f9513ccda07cd837c91ed08e00482b0251d129
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2017-02-27 15:26:55 +0100

    Update version for updating release of v3.2-r1

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016112900;        // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2017022700;        // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2014051200;        // Requires this Moodle version
 $plugin->cron      = 300;                 // Period for cron to check this module (secs)
 $plugin->maturity  = MATURITY_STABLE;
