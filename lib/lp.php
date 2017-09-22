06254714f7cb39c8bd0dc3efe9464040fdf309d2
Justus Flerlage <flerlage@math.tu-berlin.de>
2017-09-22 14:41:27 +0000

    added missing dirname(__FILE__)

--- lib/lp.php
+++ lib/lp.php
@@ -14,6 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-require_once('lp/linear_program.php');
-require_once('lp/weighters.php');
-require_once('lp/engines.php');
+require_once(dirname(__FILE__).'/lp/linear_program.php');
+require_once(dirname(__FILE__).'/lp/weighters.php');
+require_once(dirname(__FILE__).'/lp/engines.php');
