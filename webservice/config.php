3e8b622eb4a2b82338ff67e82cc1324c47fe30b6
Justus Flerlage <flerlage@math.tu-berlin.de>
2017-10-25 15:39:23 +0200

    fixed syntax error

--- webservice/config.php
+++ webservice/config.php
@@ -1,5 +1,5 @@
 <?php
 
 const RATINGALLOCATE_ENGINE = 'scip';
-const RATINGALLOCATE_LOCAL_PATH = '/tmp/file.lp'
+const RATINGALLOCATE_LOCAL_PATH = '/tmp/file.lp';
 const RATINGALLOCATE_SECRET = '';
