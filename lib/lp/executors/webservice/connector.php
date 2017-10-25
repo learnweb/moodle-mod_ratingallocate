38cb001c07100c62493a88308f15f51ad56ee70b
Justus Flerlage <flerlage@math.tu-berlin.de>
2017-10-25 12:36:18 +0000

    replaced fopen with curl

--- lib/lp/executors/webservice/connector.php
+++ lib/lp/executors/webservice/connector.php
@@ -72,42 +72,33 @@ class connector extends \ratingallocate\lp\executor {
      * @param $engine Engine that is used
      * @param $lp_file Content of the LP file
      *
-     * @throws exception 
+     * @throws exception If status code is not 200 (OK)
      *
      * @return Stream of stdout
      */
     public function solve($lp_file) {
-        $handle = fopen($this->get_uri(), 'rb', false, $this->build_request($lp_file));
+        $handle = curl_init();
 
-        if(!$handle)
-            throw new \exception('Could not connect to webservice backend!');
+        curl_setopt_array($handle, [CURLOPT_URL => $this->get_uri(),
+                                    CURLOPT_POST => true,
+                                    CURLOPT_RETURNTRANSFER => true,
+                                    CURLOPT_POSTFIELDS => ['lp_file' => $lp_file,
+                                                           'secret' => $this->get_secret()]]);
+        
+        $result = curl_exec($handle);
 
-        return $handle;
-    }
-
-    /**
-     * Builds the request for fopen
-     *
-     * @param $lp_file Content of the lp file
-     *
-     * @return Stream context
-     */
-    public function build_request($lp_file) {
-        return stream_context_create(['http' => ['method' => 'POST',
-                                                 'header' => 'Content-type: application/x-www-form-urlencoded',
-                                                 'content' => $this->build_query($lp_file)]]);
-    }
-
-    /**
-     * Build the query for the request
-     *
-     * @param $lp_file Content of the lp file
-     *
-     * @return Encoded http query
-     */
-    public function build_query($lp_file) {
-        return http_build_query(['lp_file' => $lp_file,
-                                 'secret' => $this->get_secret()]);
+        if(($status = curl_getinfo($handle, CURLINFO_HTTP_CODE)) != 200) {
+            curl_close($handle);
+            throw new \exception("Could not retrieve solution (HTTP Status $status)!");
+        }
+        
+        curl_close($handle);
+        
+        $stream = fopen('php://memory', 'r+');
+        fwrite($stream, $result);
+        rewind($stream);
+        
+        return $stream;
     }
     
 }
