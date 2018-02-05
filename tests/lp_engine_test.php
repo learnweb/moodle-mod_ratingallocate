cdf7dbf222911dac0c009b4548d3a7b413eb1020
Justus Flerlage <flerlage@math.tu-berlin.de>
2018-02-05 14:00:06 +0000

    added engine tests

--- tests/lp_engine_test.php
+++ tests/lp_engine_test.php
@@ -17,4 +17,52 @@
 defined('MOODLE_INTERNAL') || die();
 
 class lp_engine_test extends basic_testcase {
+
+    private $cplex = null;
+    private $scip = null;
+
+    public function setUp() {
+        $this->cplex = new \mod_ratingallocate\local\lp\engines\cplex();
+        $this->scip = new \mod_ratingallocate\local\lp\engines\scip();
+    }
+
+    /**
+     * @covers \mod_ratingallocate\local\lp\engines\scip::read
+     */
+    public function test_scip_read() {
+        $handle = fopen(__DIR__.'/scip.log', 'r');
+        $solution = $this->scip->read($handle);
+
+        $this->assertEquals($solution['x1'], 5);
+        $this->assertEquals($solution['x2'], 2);
+
+        fclose($handle);
+    }
+
+    /**
+     * @covers \mod_ratingallocate\local\lp\engines\cplex::read
+     */
+    public function test_cplex_read() {
+        $handle = fopen(__DIR__.'/cplex.log', 'r');
+        $solution = $this->cplex->read($handle);
+
+        $this->assertEquals($solution['x1'], 5);
+        $this->assertEquals($solution['x2'], 2);
+
+        fclose($handle);
+    }
+
+    /**
+     * @covers \mod_ratingallocate\local\lp\engines\cplex::get_command
+     */
+    public function test_cplex_command() {
+        $this->assertNotEmpty($this->cplex->get_command('file.lp'));
+    }
+
+    /**
+     * @covers \mod_ratingallocate\local\lp\engines\scip::get_command
+     */
+    public function test_scip_command() {
+        $this->assertNotEmpty($this->scip->get_command('file.lp'));
+    }
 }
