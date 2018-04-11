66bfbe17ab077abf7045a2c51c042dcf7cc5ba8b
Justus Flerlage <flerlage@math.tu-berlin.de>
2018-04-11 18:17:32 +0200

    Replaced lib.php import and added configuration varaible for enable the webservice backend

--- webservice/index.php
+++ webservice/index.php
@@ -1,12 +1,21 @@
 <?php
 
 require_once(dirname(__FILE__).'/config.php');
-require_once(dirname(__FILE__).'/../lib/lp.php');
 
-$engine_path = '\\ratingallocate\\lp\\engines\\'.RATINGALLOCATE_ENGINE;
+if(!RATINGALLOCATE_WEBSERVICE_BACKEND_ENABLED)
+    die;
+
+require_once(dirname(__FILE__).'/../classes/local/lp/engine.php');
+require_once(dirname(__FILE__).'/../classes/local/lp/engines/scip.php');
+require_once(dirname(__FILE__).'/../classes/local/lp/engines/cplex.php');
+require_once(dirname(__FILE__).'/../classes/local/lp/executor.php');
+require_once(dirname(__FILE__).'/../classes/local/lp/executors/local.php');
+require_once(dirname(__FILE__).'/../classes/local/lp/executors/webservice/backend.php');
+
+$engine_path = '\\mod_ratingallocate\\local\\lp\\engines\\'.RATINGALLOCATE_ENGINE;
 $engine = new $engine_path();
 
-$webservice = new \ratingallocate\lp\executors\webservice\backend($engine,
-                                                                  RATINGALLOCATE_SECRET);
+$webservice = new \mod_ratingallocate\local\lp\executors\webservice\backend($engine,
+                                                                            RATINGALLOCATE_SECRET);
 
 $webservice->main();
