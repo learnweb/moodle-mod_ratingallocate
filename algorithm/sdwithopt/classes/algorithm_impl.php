fc89bb678acb42be42d9014f01ecb1698385f411
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2019-03-18 14:57:44 +0100

    added emtpy skeleton for sdwithopt algorithm

---
+++ algorithm/sdwithopt/classes/algorithm_impl.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ *
+ * Contains the algorithm for the distribution
+ *
+ * @package    raalgo_sdwithopt
+ * @copyright  2019 Wwu
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace raalgo_sdwithopt;
+defined('MOODLE_INTERNAL') || die();
+
+class algorithm_impl extends \mod_ratingallocate\algorithm {
+
+    public function get_name() {
+        return 'sdwithopt';
+    }
+
+    public function compute_distribution($choicerecords, $ratings, $usercount) {
+        return array();
+    }
+}
