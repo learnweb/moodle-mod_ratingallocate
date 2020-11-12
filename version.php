ea8f263b0877045862b624260ac259ca16021cfa
Nina Herrmann <nina.herrmann@uni-muenster.de>
2020-11-12 11:33:01 +0100

    Update version.php

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020060500;        // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2020111200;        // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2017111300;        // Requires this Moodle version
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v3.9-r1';
+$plugin->release   = 'v3.9-r2';
 $plugin->component = 'mod_ratingallocate';  // To check on upgrade, that module sits in correct place
