8edea1b5ca8dcd2b645499ba2f1364827458a548
David Thompson <david.thompson@catalyst.net.nz>
2021-06-29 12:10:59 +1200

    Only update choice groups if 'usegroups' selected.

--- locallib.php
+++ locallib.php
@@ -379,7 +379,9 @@ class ratingallocate {
 
                 if (!$mform->is_cancelled()) {
                     if ($mform->is_validated()) {
-                        $this->update_choice_groups($data->choiceid, $data->groupselector);
+                        if($data->usegroups) {
+                            $this->update_choice_groups($data->choiceid, $data->groupselector);
+                        }
 
                         // Processing for editor element (FORMAT_HTML is assumed).
                         // Note: No file management implemented at this point.
