5311fbcf53931aa262a9e40529b91009c2dfe679
Dan Marsden <dan@danmarsden.com>
2022-12-01 13:59:30 +1300

    Add check as suggested by @PhMemmel

--- strategy/strategy05_order.php
+++ strategy/strategy05_order.php
@@ -105,7 +105,9 @@ class mod_ratingallocate_view_form extends \ratingallocate_strategyform {
         // Filter choices to display by groups, where 'usegroups' is true.
         $ratingdata = $this->ratingallocate->filter_choices_by_groups($ratingdata, $USER->id);
 
-        $choicecounter = $this->get_strategysetting(strategy::COUNTOPTIONS);
+        // If we have less options because of group restrictions than configured for the strategy,
+        // we have to limit it, because user cannot vote for one option multiple times.
+        $choicecounter = min($this->get_strategysetting(strategy::COUNTOPTIONS), count($ratingdata));
         $choices = array();
 
         foreach ($ratingdata as $data) {
