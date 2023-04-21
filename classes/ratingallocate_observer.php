33e4886e288db45fc564d922930098e3ab5a1bdc
Irina Hoppe <irina.hoppe@uni-muenster.de>
2023-04-21 10:14:45 +0200

    fixed namespace declaration in ratingallocate_observer.php

--- classes/ratingallocate_observer.php
+++ classes/ratingallocate_observer.php
@@ -22,8 +22,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
 namespace mod_ratingallocate;
+defined('MOODLE_INTERNAL') || die();
 
 class ratingallocate_observer {
 
