e337e6a8315cf94593e6c9388715ab6bb75cf076
Laur0r <l_troo01@uni-muenster.de>
2023-10-19 13:49:10 +0200

    bump version for release update

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023101700;        // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2023101900;        // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2020061500;         // Requires Moodle 3.9+.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'v4.3-r1';
