de61f8dbb552b67ff10f704fe86b301ea97b5f9e
Laur0r <l_troo01@uni-muenster.de>
2024-08-26 15:16:13 +0200

    version bump for v4.4-r2

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024080900;        // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2024082600;        // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2022112800;         // Requires Moodle 4.1 and higher.
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v4.4-r1';
+$plugin->release = 'v4.4-r2';
 $plugin->component = 'mod_ratingallocate';  // To check on upgrade, that module sits in correct place.
