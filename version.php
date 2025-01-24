a2915245f33149a0605d2cc9fc46e06daf057529
Daniel Meißner <daniel.meissner@uni-muenster.de>
2025-01-24 17:41:04 +0100

    Bump version for next release

--- version.php
+++ version.php
@@ -28,5 +28,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2024082600;        // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2022112800;         // Requires Moodle 4.1 and higher.
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v4.4-r2';
+$plugin->release = 'v4.4-r3';
 $plugin->component = 'mod_ratingallocate';  // To check on upgrade, that module sits in correct place.
