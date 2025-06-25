dc4ec4194a09d118a4cca03f19ea60f8e80713ec
Tamaro Walter <tamaro.walter@uni-muenster.de>
2025-06-25 19:08:00 +0200

    Fix the code formatting

--- backup/moodle2/backup_ratingallocate_activity_stepslib.php
+++ backup/moodle2/backup_ratingallocate_activity_stepslib.php
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 namespace mod_ratingallocate;
+
 defined('MOODLE_INTERNAL') || die();
 
 require_once(dirname(__FILE__) . '/backup_restore_helper.php');
