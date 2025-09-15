a3c0beb98e6657ea092751bcbe91783037364183
Daniel Meißner <daniel.meissner@uni-muenster.de>
2025-09-15 12:28:56 +0200

    Support missing get_suffix() in Moodle 4.1

--- mod_form.php
+++ mod_form.php
@@ -376,7 +376,12 @@ class mod_ratingallocate_mod_form extends moodleform_mod {
      * @return string
      */
     protected function get_suffixed_name(string $fieldname): string {
-        return "completion{$fieldname}_{$this->get_suffix()}";
+        global $CFG;
+        if ($CFG->version < 2023100900) {
+            return "completion{$fieldname}";
+        } else {
+            return "completion{$fieldname}_{$this->get_suffix()}";
+        }
     }
 
     /**
